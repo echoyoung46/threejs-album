e455056a3a829fb5f688c293c990c7f792893340
yangwt <yangwenting@meizu.com>
2016-06-17 21:07:11 +0800

    delete Babylon

--- mobile/js/mobile.js
+++ mobile/js/mobile.js
@@ -1,13 +1,14 @@
 //先引用THree.js
 
 function Mobile(){
-
+    this.mesh = null;
 }
 
 
 Mobile.prototype.init = function() {
+    this.mesh = 'I am a mesh';
     var loader = new THREE.ColladaLoader();
-    var mesh = null, isLoadFlag = false;
+    // var mesh = null, isLoadFlag = false;
 
     loader.load("./models/m80/m80_dae.dae", function (result) {
         var geom = result.scene.children[0].children[0].geometry;
@@ -15,19 +16,15 @@ Mobile.prototype.init = function() {
         var texture = THREE.ImageUtils.loadTexture('./models/m80/m80_map.jpg', {}, function() {
             var mat = new THREE.MeshLambertMaterial();
             mat.map = texture;
-            mesh = new THREE.Mesh(geom, mat);
-            mesh.scale.set(0.3, 0.3, 0.3);
-            
-            isLoadFlag = true;
+            this.mesh = new THREE.Mesh(geom, mat);
+            this.mesh.scale.set(0.3, 0.3, 0.3);
+            console.log('I am a mesh3');
         });
         
+        console.log('I am a mesh2');
     });
 
-    if(isLoadFlag) {
-        return res;
-    }else{
-        
-    }
+    this.mesh = 'I am a mesh1';
 }
 
 Mobile.prototype.isLoaded =  function(_para) {
